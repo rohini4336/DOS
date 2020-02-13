41b3d5c83bfec3c41d95eb9c4915a5c1821f9b41
Rohini <rohini@cyperts.net>
2020-02-13 14:09:11 +0530

    data dir

---
+++ biosboot/genesis/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DATADIR="./blockchain"
+
+if [ ! -d $DATADIR ]; then
+  mkdir -p $DATADIR;
+fi
+
+./nodeos \
+--signature-provider BOS61u3va4gZjoeC54eKw3KDCPhqPgEQez24i6W8ea7zxw2JYqFUY=KEY:5K7Za2svF13srBPVPxDEuzrugynJSNVNZi5zB1tHirhqhG2FFaD \
+--plugin eosio::producer_plugin \
+--plugin eosio::producer_api_plugin \
+--plugin eosio::chain_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_api_plugin \
+--plugin eosio::history_plugin \
+--data-dir $DATADIR"/data" \
+--blocks-dir $DATADIR"/blocks" \
+--config-dir $DATADIR"/config" \
+--producer-name eosio \
+--http-server-address 127.0.0.1:8888 \
+--p2p-listen-endpoint 127.0.0.1:9010 \
+--access-control-allow-origin=* \
+--contracts-console \
+--http-validate-host=false \
+--verbose-http-errors \
+--enable-stale-production \
+--p2p-peer-address localhost:9011 \
+--p2p-peer-address localhost:9012 \
+--p2p-peer-address localhost:9013 \
+>> $DATADIR"/nodeos.log" 2>&1 & \
+echo $! > $DATADIR"/eosd.pid"
